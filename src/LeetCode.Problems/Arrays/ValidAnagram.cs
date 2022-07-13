a5fe808c7525028af9f35df636327637ef5bbde0
Anders Bo Rasmussen <anras573@gmail.com>
2022-07-13 12:45:47 +0200

    Solve Valid Anagram.

---
+++ src/LeetCode.Problems/Arrays/ValidAnagram.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Problems.Arrays
+{
+    public class ValidAnagram
+    {
+        public bool Solve(string s, string t)
+        {
+            var sArray = s.ToCharArray();
+            Array.Sort(sArray);
+
+            var tArray = t.ToCharArray();
+            Array.Sort(tArray);
+
+            return sArray.SequenceEqual(tArray);
+        }
+    }
+}
---
+++ src/LeetCode.Tests/Arrays/ValidAnagramTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Problems.Arrays;
+
+namespace LeetCode.Tests.Arrays
+{
+    public class ValidAnagramTest
+    {
+        [Theory]
+        [InlineData("anagram", "nagaram")]
+        public void ReturnTrue_WhenParametersAreAnagrams(string s, string t)
+        {
+            var sut = new ValidAnagram();
+
+            var isAnagram = sut.Solve(s, t);
+
+            Assert.True(isAnagram);
+        }
+
+        [Theory]
+        [InlineData("rat", "car")]
+        public void ReturnFalse_WhenParametersAreNotAnagrams(string s, string t)
+        {
+            var sut = new ValidAnagram();
+
+            var isAnagram = sut.Solve(s, t);
+
+            Assert.False(isAnagram);
+        }
+    }
+}
