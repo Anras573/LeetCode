fe938c3f53f0362d58273c71834a55d2265edd8b
Anders Bo Rasmussen <anras573@gmail.com>
2022-07-20 19:09:40 +0200

    Solve Best Time to Buy & Sell Stock Problem.

---
+++ src/LeetCode.Problems/SlidingWindow/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Problems.SlidingWindow
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        /// <summary>
+        /// You are given an array prices where <c>prices[i]</c> is the price of a given stock on the ith day.
+        /// <br/>
+        /// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+        /// <br/>
+        /// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns>the maximum profit you can achieve from this transaction. If you cannot achieve any profit, returns 0.</returns>
+        public int Solve(int[] prices)
+        {
+            var maxProfit = 0;
+            var left = 0; // buy
+            var right = 1; // sell
+
+            while(right < prices.Length)
+            {
+                if (prices[left] < prices[right])
+                {
+                    var profit = prices[right] - prices[left];
+                    maxProfit = Math.Max(maxProfit, profit);
+                }
+                else
+                {
+                    left = right;
+                }
+
+                right++;
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ src/LeetCode.Tests/SlidingWindow/BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Problems.SlidingWindow;
+
+namespace LeetCode.Tests.SlidingWindow
+{
+    public class BestTimeToBuyAndSellStockTest
+    {
+        [Theory]
+        [InlineData(new[] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [InlineData(new[] { 7, 6, 4, 3, 1 }, 0)]
+        public void GivenArrayOfPrices_ReturnMaximumProfit(int[] prices, int expectedProfit)
+        {
+            var sut = new BestTimeToBuyAndSellStock();
+
+            var profit = sut.Solve(prices);
+
+            Assert.Equal(expectedProfit, profit);
+        }
+    
+    }
+}
